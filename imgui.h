d869d7baa3f54b74ad4683137bac66b8f00d628a
hilkojj <hilkojj@outlook.com>
2024-01-20 11:29:54 +0100

    Added DropShadow color

--- imgui.h
+++ imgui.h
@@ -1139,6 +1139,7 @@ enum ImGuiCol_
     ImGuiCol_TextDisabled,
     ImGuiCol_WindowBg,              // Background of normal windows
     ImGuiCol_WindowShadow,
+    ImGuiCol_DropShadow,
     ImGuiCol_ChildBg,               // Background of child windows
     ImGuiCol_PopupBg,               // Background of popups, menus, tooltips windows
     ImGuiCol_Border,
